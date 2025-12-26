f32d239f41cc1b7491ef03a6a0d42c40af3f59e1
ARUNKK51 <kkarun51@gmail.com>
2025-12-26 04:31:54 -0800

    added Resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,10 +1,8 @@
 import resumeimg from "../assets/resume.jpg"
+import RESUMEPDF from "../assets/ARUNKK-4.pdf"
 export default function Resume()
 {
-    const config =
-    {
-        Link :"https://pdfobject.com/pdf/sample.pdf"
-    }
+    
 
     return <section id="Resume" className="flex flex-col md:flex-row bg-secondary px-5">
 <div className="md:w-1/2 flex justify-center md:justify-end py-5">
@@ -13,7 +11,7 @@ export default function Resume()
 <div className="md:w-1/2 flex justify-center">
 <div className="flex flex-col justify-center  text-white">
 <h1 className="text-4xl border-b-blue-500 border-b-2 w-[140px] mb-5 font-bold">Resume</h1>
-<p className="pb-5">You can view my resume <a className="btn" href={config.Link}>Download</a></p>
+<p className="pb-5">You can view my resume <a className="btn" href={RESUMEPDF} target="-blank">Download</a></p>
 
 
 </div>
